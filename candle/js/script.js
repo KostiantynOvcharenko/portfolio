d6da97bf8e3b438de15eec37ad8a406d08c9fe89
Kostiantyn Ovcharenko <z101z202@gmail.com>
2024-10-30 21:04:38 +0200

    footer

--- candle/js/script.js
+++ candle/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Burger
     const burgerIcon = document.getElementById('burger-icon');
     const menu = document.getElementById('menu');
 
@@ -6,4 +8,17 @@ document.addEventListener('DOMContentLoaded', function() {
         burgerIcon.classList.toggle('active');
         menu.classList.toggle('active');
     });
+
+    // Slider
+    // const cl_buttons = '.slider__button';
+    // const buttons = document.querySelectorAll(cl_buttons);
+    
+    // buttons.forEach(function(button){
+    //     button.addEventListener('click', () => {
+    //         buttons.forEach(function(button){
+    //             button.classList.remove('active');
+    //         });
+    //         button.classList.add('active');
+    //     });
+    // });
 });
